2ec8a8d46ba7ec59b146cb1000acfab61a915f5c
nicepero <156625434+nicepero@users.noreply.github.com>
2024-01-16 15:45:10 +0200

    Create USER.py

---
+++ GW2X/USER.py
@@ -0,0 +1,28 @@
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, D M M \n
+\n\n
+ASCENDED COMMANDER : \n
+RZ, NMS, NH, MDB, JS, JA, FC, RB, VR, MDG, RH, PLC, FOX, SN, OYI, 
+WG, ES, MQ, RD, BK, NK, TT, AC, RP, IV, JX, RS, AKM, FRT, SP, VL, 
+FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM, 
+CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR, EZP,
+AZ, CB, DY, KM, ACT, PM, AB, SX, AB, RP, DN, NN, AS, AF, VM, TS, CT,
+KG, TX, DP, NEO, KL, MT, KN, DMR, TZ, HB, VC, MK, NN, YC, MC, IS,
+SS, XK, AK \n
+============================\n
+
+consider donating to keep this project alive\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["d759f1cb88","test"]
+
+
+#=============
+#==============
